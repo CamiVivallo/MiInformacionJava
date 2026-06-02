974fc80fcd5658647be687ec6ab83bbdc6bfd7e7
Cami <cvivallo@konko.cl>
2026-06-01 21:08:50 -0400

    Subo mi tarea a github

---
+++ MiInformacion.java
@@ -0,0 +1,8 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Camila Javiera Vivallo Sainz");
+        System.out.println("Tengo 32 años de edad");
+        System.out.println("Soy de Chile y vivo en la ciudad de Santiago");
+        System.out.println("Me gusta mucho leer y ver películas");
+    }
+}
